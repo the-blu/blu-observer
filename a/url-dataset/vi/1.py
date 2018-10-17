be6a6fb5014bcdfe8dd2584732679d2eae8aca2b
변지연 <jee@beulluhaeg-ui-MacBook-Pro.local>
2018-10-17 17:17:05 +0900

    [WIP]black_detector

--- a/url-dataset/vi/1.py
+++
@@ -1,9 +0,0 @@
-f=open('data5.txt','r')
-f2=open('1.txt','w')
-fdata = f.readlines()
-
-
-for i in fdata:
-    f2.write("['" + i[9:10] + "']" + ', ')
-
-
--- b/set.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-head -n 6066 ./a/data1_final_t.txt > ./b/train3.txt
-head -n 6066 ./a/data1_final_f.txt > cat>>./b/train3.txt
-tail -n 2022 ./a/data1_final_t.txt > ./b/test3.txt
-tail -n 2022 ./a/data1_final_f.txt > cat>>./b/test3.txt
